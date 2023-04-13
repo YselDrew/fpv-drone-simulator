32c52f81a5fb8f8a67594bbf0bd83a2b108e0f8b
andryshonok98 andryshonok98 <kyselov.andrii@gmail.com>
2023-04-13 23:50:55 +0300

    add toggle drone

--- Assets/_Project/Scripts/DroneSwitch.cs
+++ Assets/_Project/Scripts/DroneSwitch.cs
@@ -6,16 +6,30 @@ public class DroneSwitch : MonoBehaviour {
   [SerializeField] GameObject scoutDrone;
   [SerializeField] GameObject kamikazeDrone;
 
+  bool isDroneActive = true;
+
   void Start() {
-    scoutDrone.SetActive(true);
+    activateDrone();
   }
 
   void Update() {
     if (Input.GetKeyDown(KeyCode.E)) SwitchDrone();
   }
 
+  void activateDrone() {
+    scoutDrone.SetActive(isDroneActive);
+    kamikazeDrone.SetActive(!isDroneActive);
+  }
+
   void SwitchDrone() {
-    scoutDrone.SetActive(false);
-    kamikazeDrone.SetActive(true);
+    /* 
+      Toggle is only for development purposes.
+      In final version you won't be able
+      switch back until kamikaze drone will be 
+      destroyed
+    */
+    isDroneActive = !isDroneActive;
+
+    activateDrone();
   }
 }
